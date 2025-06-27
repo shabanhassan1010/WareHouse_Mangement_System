403fcb099d86fed4de225a92dac53691a8b1dc13
Shaban Hassan <shabanhassan1010@gmail.com>
2025-06-27 23:10:48 +0300

    add footer and navbar and also and template for login and register

--- src/app/Layouts/auth-layout/auth-layout.ts
+++ src/app/Layouts/auth-layout/auth-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavAuth } from "../../Components/nav-auth/nav-auth";
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "../../Components/footer/footer";
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [NavAuth , RouterOutlet],
+  imports: [NavAuth, RouterOutlet, Footer],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css'
 })
--- src/app/Layouts/blank-layout/blank-layout.ts
+++ src/app/Layouts/blank-layout/blank-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBlank } from "../../Components/nav-blank/nav-blank";
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "../../Components/footer/footer";
 
 @Component({
   selector: 'app-blank-layout',
-  imports: [NavBlank , RouterOutlet],
+  imports: [NavBlank, RouterOutlet, Footer],
   templateUrl: './blank-layout.html',
   styleUrl: './blank-layout.css'
 })
